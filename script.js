4c661706b86c47d469a57683773ff0f7ef1a84cb
leskovetsuu <uliyaleskovets2012@yandex.ru>
2023-04-24 23:57:52 +0300

    Домашнее задание №4
    1. Добавила ввод списка покупок через Enter. 2. при клике на элемент списка он зачеркивается и наоборот. 3.поле ввода всегда пустое

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
-const inputWrapper=document.querySelector('.input-wrapper');
 const sendInput=document.querySelector('#input');
 const sendItems=document.querySelector('.items');
-const newItems=document.createElement('div');
-newItems.textContent=inputWrapper;
-newItems.addEventListener('keydown', function(event){
-if (event.keyCode == 'Enter'){
-   sendItems=newItems.value;
-}
-})
-inputWrapper.addEventListener('click', function(){
-   const newMMassege=sendInput.value;
-})
 sendInput.addEventListener('keydown', function(event) {
-if (event.keyCode == 'Enter'){
-newMMassege.click();
-}
-});
+if (event.key == 'Enter'){
+  const massageText = sendInput.value;
+  
+  const newText = document.createElement('div');
+  newText.textContent = massageText;
+  newText.addEventListener('click', function(){
+   newText.classList.toggle('done');
+  })
+  if (massageText != '') {
+   sendItems.append(newText);
+  }
+  sendInput.value = '';
+  }
+})
+
 
 
 /* Пустые элементы не должны добавляться */
