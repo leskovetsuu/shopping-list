e035c8d079c43d7713a0203ec4d1f9d18014f13f
leskovetsuu <uliyaleskovets2012@yandex.ru>
2023-04-27 23:11:14 +0300

    домашнее задание 4
    добавление новых элементов через кнопку, нажатие на эл-т и он зачеркивается и наоборот. пустая строка не отправляется.

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+const sendInput=document.querySelector('#input');
+const sendItems=document.querySelector('.items');
+sendInput.addEventListener('keydown', function(event) {
+if (event.key == 'Enter'){
+  const massageText = sendInput.value;
+  
+  const newText = document.createElement('div');
+  newText.textContent = massageText;
+  newText.addEventListener('click', function(){
+   newText.classList.toggle('done');
+  })
+  if (massageText != '') {
+   sendItems.append(newText);
+  }
+  sendInput.value = '';
+  }
+})
+
 
 /* Пустые элементы не должны добавляться */
 
